ebdc5a642da6c73a4ad130afb8ff1740f3a7e011
zeromrja <129565385+zeromrja@users.noreply.github.com>
2025-07-03 20:48:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configura la escena, la cámara y el renderizador
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 10000);
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
